c91565c291aed7e4d723eccd888d5ebaedfca235
lilinj2000 <lilinj2000@gmail.com>
2017-08-03 08:16:00 +0000

    fixed env.sh

--- src/env.sh
+++ src/env.sh
@@ -1,13 +1,10 @@
 
 home_3rd=~/3rd
 
-if test -d /llj/3rd ; then
-   home_3rd=/llj/3rd
-fi
-
 home_boost=$home_3rd/boost
-home_zeusing=$home_3rd/ZeusingApi_20150331
+home_uft=$home_3rd/UFT_API_Release_1.0.0.1-build_20170731
 
-export LD_LIBRARY_PATH=$home_boost/lib:$home_zeusing
+export LD_LIBRARY_PATH=$home_boost/lib:$home_uft/lib
+
 
 
